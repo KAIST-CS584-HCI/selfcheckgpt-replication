f345918dda0111c40d9b82d812f78c6c3892de74
Cotidie <daily142857@gmail.com>
2026-04-18 04:41:04 +0900

    feat: generated text instance

--- replication/entity/__init__.py
+++ replication/entity/__init__.py
@@ -1,5 +1,6 @@
 from .passage_instance import PassageInstance
 from .passage_result import PassageResult
 from .prompt_instance import PromptInstance
+from .generated_text_instance import GeneratedTextInstance
 
-__all__ = ["PassageInstance", "PassageResult", "PromptInstance"]
+__all__ = ["PassageInstance", "PassageResult", "PromptInstance", "GeneratedTextInstance"]
