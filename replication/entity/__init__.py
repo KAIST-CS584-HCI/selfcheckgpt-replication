778409fb2041f073304b45815acf4be727946ac0
Cotidie <daily142857@gmail.com>
2026-04-19 03:33:51 +0900

    fix: invalid class name

--- replication/entity/__init__.py
+++ replication/entity/__init__.py
@@ -1,7 +1,7 @@
-from .passage_original_instance import PassageInstance
+from .passage_original_instance import PassageOriginalInstance
 from .passage_generated_instance import PassageGeneratedInstance
 from .passage_result import PassageResult
 from .prompt_instance import PromptInstance
 from .generated_text_instance import GeneratedTextInstance
 
-__all__ = ["PassageInstance", "PassageGeneratedInstance", "PassageResult", "PromptInstance", "GeneratedTextInstance"]
+__all__ = ["PassageOriginalInstance", "PassageGeneratedInstance", "PassageResult", "PromptInstance", "GeneratedTextInstance"]
