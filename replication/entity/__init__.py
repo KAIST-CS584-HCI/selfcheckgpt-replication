b776b595144a053c98032ae85898e3b178cf8ec1
Cotidie <daily142857@gmail.com>
2026-04-19 12:37:14 +0900

    refactor: integrate passage_original and passage_generated

--- replication/entity/__init__.py
+++ replication/entity/__init__.py
@@ -1,7 +1,6 @@
-from .passage_original_instance import PassageOriginalInstance
-from .passage_generated_instance import PassageGeneratedInstance
+from .passage_instance import PassageInstance
 from .passage_prompt_result import PassagePromptResult
 from .prompt_instance import PromptInstance
 from .generated_text_instance import GeneratedTextInstance
 
-__all__ = ["PassageOriginalInstance", "PassageGeneratedInstance", "PassagePromptResult", "PromptInstance", "GeneratedTextInstance"]
+__all__ = ["PassageInstance", "PassagePromptResult", "PromptInstance", "GeneratedTextInstance"]
--- replication/entity/passage_instance.py
+++ replication/entity/passage_instance.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 
 @dataclass
-class PassageGeneratedInstance:
+class PassageInstance:
     """One passage from dataset-generated-samples-gpt-3.5-turbo.json."""
     wiki_bio_test_idx: int
     main_passage:      str
@@ -12,7 +12,7 @@ class PassageGeneratedInstance:
     wiki_bio_text:     str
 
     @classmethod
-    def from_dict(cls, item: dict) -> "PassageGeneratedInstance":
+    def from_dict(cls, item: dict) -> "PassageInstance":
         return cls(
             wiki_bio_test_idx = item['wiki_bio_test_idx'],
             main_passage      = item['main_passage'],
@@ -21,3 +21,12 @@ class PassageGeneratedInstance:
             sample_passages   = item['sample_passages'],
             wiki_bio_text     = item['wiki_bio_text'],
         )
+
+    @classmethod
+    def from_original_dict(cls, item: dict) -> "PassageInstance":
+        return cls.from_dict({
+            **item,
+            'main_passage':    item['gpt3_text'],
+            'main_sentences':  item['gpt3_sentences'],
+            'sample_passages': item['gpt3_text_samples'],
+        })
